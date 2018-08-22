3444ee4065a780e0c3c27dcadc05329a58694ab9
Michael Arrington <michaelarringtoniphone@gmail.com>
2018-08-22 12:09:19 -0700

    Update timesheet. Start revisions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import VueUi from "@vue/ui";
 import App from "./App.vue";
 import router from "./router";
+import "@vue/ui/dist/vue-ui.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueUi);
 
 new Vue({
   router,
